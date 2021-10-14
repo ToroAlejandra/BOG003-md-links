0aa3b320c80a316274391fa935044848baec0541
ToroAlejandra <jessicaalejandratc@gmail.com>
2021-10-14 17:26:18 -0500

    Consumo de promesas e inicio de la funcion md-links

---
+++ README1.md
@@ -0,0 +1,18 @@
+
+[Markdown](https://es.wikipedia.org/wiki/Markdown) es un lenguaje de marcado
+ligero muy popular entre developers. Es usado en muchísimas plataformas que
+manejan texto plano (GitHub, foros, blogs, ...), y es muy común
+encontrar varios archivos en ese formato en cualquier tipo de repositorio
+(empezando por el tradicional `README.md`).
+
+Dentro de una comunidad de código abierto, nos han propuesto crear una
+herramienta usando [Node.js](https://nodejs.org/), que lea y analice archivos
+en formato `Markdown`, para verificar los links que contengan y reportar
+
+![md-links](https://user-images.githubusercontent.com/110297/42118443-b7a5f1f0-7bc8-11e8-96ad-9cc5593715a6.jpg)
+
+## 2. Resumen del proyecto
+
+[Node.js](https://nodejs.org/es/) es un entorno de ejecución para JavaScript
+construido con el [motor de JavaScript V8 de Chrome](https://developers.google.com/v8/).
+Esto nos va a permitir ejecutar JavaScript en el entorno del sistema operativo, [Node.js](https://nodejs.org/es/) 
--- index.js
+++ index.js
@@ -1,79 +1,147 @@
+#!/usr/bin/env node
 
-const fs = require('fs');
-const path = require('path');
-let MarkdownIt = require('markdown-it'),
-    md = new MarkdownIt();
-let result = md.parse(' * [Arreglos](https://curriculum.laboratoria.la/es/topics/javascript/04-arrays)');
-let verifyResult = result.slice('<a');
+const fs = require("fs");
+const { resolve } = require("path");
+const path = require("path");
+const mdLinkExtractor = require('markdown-link-extractor');
+let MarkdownIt = require("markdown-it"),
+  md = new MarkdownIt();
+
+let result = md.parse(
+  " * [Arreglos](https://curriculum.laboratoria.la/es/topic/topics/javascript/04-arrays)"
+);
+
 let filterResult;
+let filterFiles;
 
-/* filterResult = result.filter((e) => {
-  if(e.type === 'inline'){
-    return true;
-  }else{
-   return false;
-  }
-});*/
-
-filterResult = result.filter(e => e.type == 'inline');
+/* filterResult = result.filter(e => e.type == 'inline');
 
 filterResult.forEach((e) => {
-  console.log(e.children[0].attrs[0][1])
+  console.log(e.children[0].attrs[0][1]) // link
   // console.log(e.children[1].content);
-});
+}); */
 
-console.log(filterResult);
+// console.log(filterResult);
 
-//console.log(result.indexOf('"') + '  ' + result)
-
-const pathsToCheck = ['./MDLINKS', './MDLINKS/BOG003-md-links/index.js'];
-
-const absolutePath = (inputPath) => { // if path is relative this functions convert this to absolute
+const absolutePath = (inputPath) => {
+  // if path is relative this functions con>
   let resolvePath = inputPath;
-  if(!path.isAbsolute(inputPath)){
-    resolvePath = path.resolve(inputPath)
+  if (!path.isAbsolute(inputPath)) {
+    resolvePath = path.resolve(inputPath);
   }
   return resolvePath;
 };
 
-// verify if path is directory or file
-const verifyIsDirectory = (inputPath) => (fs.stat('./', (err, stats) => {
-      if (err) throw err;
-      // console.log(`stats: ${JSON.stringify(stats)}`);
-  
-      
-        return (stats.isDirectory());
-    })
-)
+const verifyIsDirectory = (inputPath) => {
+  return new Promise((resolve, reject) => {
+    fs.stat(inputPath, (err, stats) => {
+      if (err){
+        reject("No se puede leer directorio o archivo "+inputPath);
+      } else {
+        resolve(stats.isDirectory());
+      }
+    });
+  });
+};
 
-console.log(verifyIsDirectory('./'));
-
-const isMd = (inputPath) => { // 
-  if (path.extname(inputPath) === '.md') {
+const isMd = (inputPath) => {
+  //
+  if (path.extname(inputPath) === ".md") {
     return true;
-  }else{
+  } else {
     return false;
   }
 };
 
-const readFile = (inputPath) => {
-  if(isMd(inputPath)){
-    fs.readFile(inputPath, 'utf8', (err, data) => {
-      if (err) {
-        return console.error(err)
+const readDir = (inputPath) => {
+  return new Promise((resolve, reject) => {
+    fs.readdir(inputPath, (err, files) => {
+      if (err){
+        reject(err);
+      } else {
+        resolve(files);
       }
-      data.split('\n').forEach((e) => { 
-        console.log(e);
-      });
-    })
-  }
+    });
+  });
 };
 
-// readFile('./README.md');
-
-
-
-/*module.exports = (path, options) => {
-  console.log('Hola');
+const readFile = (inputPath) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(inputPath, "utf8", (err, data) => {
+      if (err){
+        reject("No pudo ser leido el archivo: "+ inputPath);
+      } else {
+        resolve(data);
+      }
+    });
+  });
 };
-*/
+
+const findLinks = (file) => {
+  return new Promise((resolve, reject) => {
+    filterResult = file.filter(e => e.type == 'inline');
+    resolve(filterResult);
+    /* filterResult.forEach((e) => {
+      if (e.children[0].attrs !== null) {
+        console.log("no es nulo");
+        resolve(e.children[0]);
+      } else {
+        reject('No ha sido posible encontrar los datos');
+      }
+    }); */
+  });
+};
+
+// findLinks("./README1.md");
+
+const mdLinks = (path, options) => {
+  verifyIsDirectory(absolutePath(path))
+  .then((res) => {
+    console.log(res);
+    if (res) {
+      return readDir(path) //dir
+    } else {
+      return [path];
+    }
+  })
+  .then((fileList) => {
+    // filtar solo archivos .md
+    return fileList.filter((file) => isMd(file) === true);
+  })
+  .then((mdFiles)=> Promise.all(mdFiles.map(mdFile => readFile(mdFile))))
+  .then((contentArray) => {
+    return contentArray.map((content) => {
+      return mdLinkExtractor(content, true);
+    });
+    // extraer los links
+  })
+  .then((multipleArrayLinks) => {
+    return multipleArrayLinks.flatMap((arrayLink) => arrayLink);
+  })
+  .then((arrayLink) => {
+
+    console.log(arrayLink);
+  })
+  /* .then((res)=>{
+    res.forEach(e => {
+      
+      e.children.forEach((a, i) => {
+        if(a.type === 'link_open'){
+          console.log(a.attrs[0][1]); //devuelve el link
+          console.log();
+          console.log(Object.keys(e).indexOf('type'),' : ', i);
+        }
+        // console.log(Object.keys(a)/* Object.keys(a).indexOf('type') );
+      })
+      //console.log(e.children.type);
+    });
+  }) */
+  .then((res)=>{
+    console.log("ultimo: ",(res));
+  })
+  .catch((err) => {
+    console.log("Error: "+err);
+  });
+} 
+
+mdLinks('./');
